569e7268a5c5a3f5081a23a5bf583339d1f53805
Pedro Meléndez <43736134+Pipolk@users.noreply.github.com>
2022-07-05 14:25:11 -0600

    Update TO_CHAR_FORMAT_ELEMENTS_UDF

--- UDFs/Teradata/DAY_OF_WEEK_UDF.sql
+++ UDFs/Teradata/DAY_OF_WEEK_UDF.sql
@@ -1,3 +1,19 @@
+-- <copyright file="DAY_OF_WEEK_UDF.sql" company="Mobilize.Net">
+--        Copyright (C) Mobilize.Net info@mobilize.net - All Rights Reserved
+-- 
+--        This file is part of the Mobilize Frameworks, which is
+--        proprietary and confidential.
+-- 
+--        NOTICE:  All information contained herein is, and remains
+--        the property of Mobilize.Net Corporation.
+--        The intellectual and technical concepts contained herein are
+--        proprietary to Mobilize.Net Corporation and may be covered
+--        by U.S. Patents, and are protected by trade secret or copyright law.
+--        Dissemination of this information or reproduction of this material
+--        is strictly forbidden unless prior written permission is obtained
+--        from Mobilize.Net Corporation.
+-- </copyright>
+
 -- ======================================================================
 -- RETURNS THE DAY OF THE WEEK A TIMESTAMP BELONGS TO, HAS THE SAME BEHAVIOR AS THE TD_DAY_OF WEEK FUNCTION FROM TERADATA
 -- PARAMETERS:
